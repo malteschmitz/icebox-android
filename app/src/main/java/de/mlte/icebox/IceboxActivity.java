7b166ce7517ea30ef7717e865a42f08fb2f6e72e
Malte Schmitz <malte@schmitz-sh.de>
2016-07-09 19:59:26 +0200

    Reorganize UI

--- app/src/main/java/de/mlte/icebox/IceboxActivity.java
+++ app/src/main/java/de/mlte/icebox/IceboxActivity.java
@@ -13,7 +13,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.goebl.david.Webb;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -22,8 +25,11 @@ import com.google.zxing.integration.android.IntentResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import de.mlte.icebox.model.Drink;
 import de.mlte.icebox.model.Serializer;
@@ -33,12 +39,37 @@ public class IceboxActivity extends AppCompatActivity {
     Webb webb;
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DrinkTask().execute();
+        }
+    }
+
+    private List<Drink> drinks = Collections.EMPTY_LIST;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icebox);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (IceboxActivity.this.drinks.size() > position) {
+                    Drink drink = IceboxActivity.this.drinks.get(position);
+                    Log.d("mlte", drink.getName() + " clicked");
+                }
+            }
+        });
 
         // create the client (one-time, can be used from different threads)
         webb = Webb.create();
@@ -55,28 +86,17 @@ public class IceboxActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                Log.d("mlte", "Settings icon clicked");
+                break;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.action_scan:
+                IntentIntegrator integrator = new IntentIntegrator(this);
+                integrator.initiateScan();
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void getDrinks(View view) throws JSONException {
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            new DrinkTask().execute();
-        } else {
-            //TODO: display error
-        }
     }
 
     private class DrinkTask extends AsyncTask<Void, Void, List<Drink>> {
@@ -96,24 +116,33 @@ public class IceboxActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(List<Drink> drinks) {
-            Intent intent = new Intent(IceboxActivity.this, DrinksActivity.class);
-            Drink[] drinkArray = drinks.toArray(new Drink[drinks.size()]);
-            intent.putExtra(DRINKS_MESSAGE, drinkArray);
-            startActivity(intent);
-        }
-    }
+            //Intent intent = new Intent(IceboxActivity.this, DrinksActivity.class);
+            //Drink[] drinkArray = drinks.toArray(new Drink[drinks.size()]);
+            //intent.putExtra(DRINKS_MESSAGE, drinkArray);
+            //startActivity(intent);
 
-    public void scan(View view) {
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.initiateScan();
+            IceboxActivity.this.drinks = drinks;
+
+            List<Map<String, String>> drinksList = new ArrayList<>(drinks.size());
+            for (Drink drink: drinks) {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("name", drink.getName());
+                map.put("price", String.format("EUR %.2f", drink.getFullprice() / 100d));
+                drinksList.add(map);
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(IceboxActivity.this, drinksList, R.layout.row_drink, new String[]{"name", "price"}, new int[]{R.id.name, R.id.price});
+
+            ListView listView = (ListView) findViewById(R.id.list);
+            listView.setAdapter(simpleAdapter);
+        }
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanResult != null) {
             String re = scanResult.getContents();
-            EditText edit = (EditText) findViewById(R.id.edit_message);
-            edit.setText(re);
+            Log.d("mlte", "Scan: " + re);
         }
     }
 }
